eeb0d99840b0d02857404db596c200be1b26bf77
Ivan <ivasha-89@mail.ru>
2019-04-24 17:05:23 +0300

    stats time fix

--- resources/views/slbs/stats.blade.php
+++ resources/views/slbs/stats.blade.php
@@ -5,7 +5,7 @@
         <div class="alert alert-info shadow">
             <p class="h4" id="timeSet">
                 Статистика за {{$diff-1}} дней начиная с {{ $date[0]->format(' d ') . $months[$date[0]->format('n')] }}
-                до {{ $date[$diff-2]->format(' d ') . $months[$date[$diff-2]->format('n')] }} в %
+                до {{ $date[$diff-1]->format(' d ') . $months[$date[$diff-1]->format('n')] }} в %
             </p>
         </div>
     </div>
@@ -67,7 +67,7 @@
             </div>
             <div class="col-6">
                 <div class="float-left">
-                    <input class="form-control" type="date" name="dateEnd" value="{{$date[$diff-2]->format('Y-m-d')}}" min="{{$date[1]->format('Y-m-d')}}"
+                    <input class="form-control" type="date" name="dateEnd" value="{{$date[$diff-1]->format('Y-m-d')}}" min="{{$date[1]->format('Y-m-d')}}"
                            onchange="this.form.submit()">
                 </div>
             </div>
