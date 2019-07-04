d734403c7ca0357f8ae01bfbdf07c3b3dc440160
Ivan <ivasha-89@mail.ru>
2019-07-04 19:45:38 +0300

    personal page view fix

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,7 +22,7 @@
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ url('css/bootstrap.min.css') }}">
-    <link rel="stylesheet" href="{{ url('css/bootstrap-reboot.min.css') }}">
+{{--    <link rel="stylesheet" href="{{ url('css/bootstrap-reboot.min.css') }}">--}}
 </head>
 <body>
 <div class="spinner-border text-success"
