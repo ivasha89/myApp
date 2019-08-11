f554a281eb7f7060204c7ace60e52bb17fa14f8a
Ivan <ivasha-89@mail.ru>
2019-08-11 17:56:44 +0300

    lil fix

--- public/js/statistic.js
+++ public/js/statistic.js
@@ -18,5 +18,6 @@ $(document).ready(function () {
         $('.img').removeClass('w-25').addClass('w-50');
         $('.list-item').removeClass('w-75').addClass('w-50');
         $('.all').removeClass('w-50');
+        $('.all-img').addClass('rounded-circle');
     }
 });
--- resources/views/user/all.blade.php
+++ resources/views/user/all.blade.php
@@ -5,15 +5,15 @@
 @endsection
 
 @section('content')
-    <div class="list-group all w-50">
+    <div class="list-group all w-25 card ml-5">
         @foreach($users as $user)
             <div class="list-group-item d-flex flex-row p-1">
-                <div class="w-25">
-                    <img src="{{ url('/svg/'.$user->id.'.jpg')}}" width="55"
-                     class="img-thumbnail rounded-circle" alt="...">
+                <div class="w-25 d-flex justify-content-center mr-3">
+                    <img src="{{ url('/svg/'.$user->id.'.jpg')}}" width="100"
+                     class="img-thumbnail all-img" alt="...">
                 </div>
                 <div class="w-75 align-self-center">
-                    <a href='{{ url("/$user->id") }}' class="text-dark">
+                    <a href='{{ url("/$user->id") }}' class="btn btn-outline-info" role="button">
                         @if($user->sname)
                             {{ $user->sname }}
                         @else
